45bc16a36be467dcda887693a75348ac0f9c1376
philipgher <32325241+philipgher@users.noreply.github.com>
2021-04-14 18:21:36 +0200

    Sort incrementally

--- utils/citiesList/findCityOnInput.js
+++ utils/citiesList/findCityOnInput.js
@@ -4,7 +4,7 @@ const findCityOnInput = (input) => {
 	return cities.filter(city => (
 		city.name.toLowerCase().startsWith(input.toLowerCase())
 		|| city.alternativeNames.some(name => name.toLowerCase().startsWith(input.toLowerCase()))
-	)).sort((a, b) => a.population - b.population);
+	)).sort((a, b) => b.population - a.population);
 };
 
 export default findCityOnInput;
